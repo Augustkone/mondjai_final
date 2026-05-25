16afcea930313d90f12c1bf645b89b03353c281c
Augustkone <augustinkone272@gmail.com>
2026-05-25 18:11:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ except ImportError:
 # CONFIGURATION GEMINI API
 GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY', '')
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash-latest')
+model = genai.GenerativeModel('gemini-1.0-pro')
 def generer_code():
     return ''.join(random.choices(string.digits, k=6))
 
